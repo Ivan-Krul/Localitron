d10e4ec75c0ea1cb3e259c660cc2ec9d1d9e2d1d
Ivan-Krul <kiwiivanya128@gmail.com>
2024-05-09 09:52:44 +0200

    made lister more usable

--- src/main/Lister/Lister.ts
+++ src/main/Lister/Lister.ts
@@ -1,34 +1,62 @@
 
 import { LinkChopper } from "../Utils/LinkChopper";
 
-export class ListerElement {
+export class Folder {
     public name: string;
-    public path: string[];
 
-    constructor(name: string, path: string[]) {
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+export class Music {
+    public name: string;
+    public path: Folder[];
+
+    constructor(name: string, path: Folder[]) {
         this.name = name;
         this.path = path;
     }
 }
 
 export class Lister {
+    private origin: string;
+    private musicList: Music[];
 
+    public constructor(origin: string, globalList: string[]) {
+        this.origin = origin;
+        this.musicList = [];
 
-    private globalList: ListerElement[];
+        this.setNewOrigin(origin, globalList);
+    }
 
+    public setNewOrigin(origin: string, globalList: string[]) {
+        this.origin = origin;
+        this.musicList = [];
 
+        for(let i = 0; i < globalList.length; i++) {
+            const musicPath = globalList[i];
+            
+            const musicFolders = LinkChopper.cropDir(musicPath);
+            let folders: Folder[] = [];
 
-    public constructor(globalPathList: string[]) {
-        this.globalList = [];
+            for(let f = 0; f < musicFolders.length; f++) {
+                folders.push(new Folder(musicFolders[f]));
+            }
 
-        this.generateElements(globalPathList);
-    }    
-
-    private generateElements(globalPathList: string[]) {
-        for(let i = 0; i < globalPathList.length; i++) {
-            let liEl = new ListerElement(LinkChopper.cropName(globalPathList[i]),LinkChopper.cropDir(globalPathList[i]));
-
-            this.globalList.push(liEl);
+            this.musicList.push(new Music(LinkChopper.cropName(musicPath), folders));
         }
+    }
+
+    public getMusicLength(){
+        return this.musicList.length;
+    }
+
+    public getMusic(index: number) {
+        return this.musicList[index];
+    }
+
+    public getOrigin() {
+        return this.origin;
     }
 }
